3215868a6aedc99c918dc5e1c873714709792f7f
Alex <alexatrs@hotmail.co.uk>
2021-02-10 12:24:55 +0000

    Created json server and used useEffect to fetch the data

--- website/src/Home.js
+++ website/src/Home.js
@@ -1,22 +1,23 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        {title: 'My new website', body: 'Lorem ipsum....', author: 'Alex', id: 1 },
-        {title: 'Welcome party', body: 'Lorem ipsum....', author: 'Dave', id: 2 },
-        {title: 'Web Dev top tips', body: 'Lorem ipsum....', author: 'Alex', id: 3 }
-    ]);
+    const [blogs, setBlogs] = useState(null);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs)
-    }
+    useEffect(() => {
+        fetch('http://localhost:8000/blogs')
+        .then(res => {
+            return res.json()
+        })
+        .then((data) => {
+            setBlogs(data)
+        })
+    }, []);
 
     return ( 
         <div className="home">
-            <BlogList blogs={blogs} title="Alex Blogs" handleDelete={handleDelete} />
+            {blogs && <BlogList blogs={blogs} title="Alex Blogs" />}
         </div>
      );
 }
