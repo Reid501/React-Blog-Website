9a600e31d77e7f1a32eb4fae97d2f86fe9928712
Alex <alexatrs@hotmail.co.uk>
2021-02-11 11:50:21 +0000

    Router links added, blog pages added and useEffect cleanup added

--- website/src/App.js
+++ website/src/App.js
@@ -1,15 +1,30 @@
 import NavBar from './NavBar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Create from './Create';
+import BlogDetails from './BlogDetails';
 
 function App() {
 
   return (
-    <div className="App">
-      <NavBar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route exact path="/create">
+              <Create />
+            </Route>
+            <Route exact path="/blogs/:id">
+              <BlogDetails />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
